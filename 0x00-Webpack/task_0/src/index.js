8280bfb260ff2d1688d2654b399792ce04c30a30
Daniel Obembe <danielobembe1@gmail.com>
2023-01-28 04:58:37 +0100

    fixed the sixth error

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
-import $ from "jquery";
+"use strict";
+const $ = require("jquery");
 
-$(function () {
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
-});
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");

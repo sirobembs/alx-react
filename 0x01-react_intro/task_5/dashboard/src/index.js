0ac190e3b9090675c67938e9c0d7095851f7e82f
Daniel Obembe <danielobembe1@gmail.com>
2023-02-11 12:37:07 +0100

    webpack stle

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App/App";
 import Notifications from "./Notifications/Notifications";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <div className="root-notifications">
       <Notifications />
     </div>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
